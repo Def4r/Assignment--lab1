774192b667270e025c1317cf45557e04ac88cddc
Def4r <danielnumba1@icloud.com>
2023-10-02 12:33:17 -0400

    Add another Dice method to the DiceMod file

--- DiceMod.py
+++ DiceMod.py
@@ -5,7 +5,7 @@ import random
 # Methods for the dices
 
 def diceI():
-   onlyDice = random.randomint(1,6)
+   onlyDice = random.randint(1,6)
    print("You rolled ", onlyDice)
    print("")
    return onlyDice
@@ -14,11 +14,22 @@ def dicesII():
    firstDice = random.randint(1, 6)
    secondDice = random.randint(1, 6)
    DualDice = firstDice + secondDice
-   print("You rolled ", firstDice, " and ", secondDice, " which is ", DualDice)
+   print("You rolled a ", firstDice, " and a ", secondDice, " which is ", DualDice)
    print("")
    return DualDice
 
-diceI()
-dicesII()
+def pickDice():
+   dice_1 = random.randint(1,6)
+   dice_2 = random.randint(1,6)
+   FinalDice = int(input("You rolled a ", dice_1, " and a ", dice_2, "Type 1 to pick Dice 1 roll or Type 2 to pick Dice 2 roll: " ))
+   if FinalDice == 1:
+      print("You picked Dice 1 which rolled ", dice_1)
+      return dice_1
+   if FinalDice == 2:
+      print("You picked Dice 2 which rolled ", dice_2)
+      return dice_2
 
+#diceI()
+#dicesII()
+pickDice()
 
