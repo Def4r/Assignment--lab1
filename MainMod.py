c3403ca57cf9a56bb40360e30ba90c9b24b8080d
Def4r <danielnumba1@icloud.com>
2023-10-03 11:02:44 -0400

    added a typewriting effect to the text

--- MainMod.py
+++ MainMod.py
@@ -1,12 +1,20 @@
 import time
 import random
+import sys
+from time import sleep
 
 def Introduction():
-    print("Welcome to Dungeon Hunters")
+    Title = "Welcome to Dungeon Hunters"
+    for char in Title:
+        sleep(0.1)
+        sys.stdou.write(char)
+        sys.stdou.flush
     time.sleep(2)
     ContinueGame = str(input("Type 'PLAY' start the game skipping the intro and tutorial. Type 'LEARN' if you want to read the tutorial and intro of the game "))
     
     if ContinueGame.lower() == "play":
         return 1
     if ContinueGame.lower() == "learn":
-        print("The objective of the game is to make it out of the dungeon")
+        print("The objective of the game is to make it out of the dungeon ALIVE.")
+        time.sleep(2)
+        print("You will have to pick between a varity of characters. They all have different stats and attributes before you choose you character you will see a breif overview of them")
