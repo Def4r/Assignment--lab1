717932c88bc436f8ecbc01a933f09464ea3954b5
Def4r <danielnumba1@icloud.com>
2023-10-03 11:16:16 -0400

    seperating parts of the method test them easier

--- MainMod.py
+++ MainMod.py
@@ -9,14 +9,7 @@ def Introduction():
         sys.stdout.write(char)
         sys.stdout.flush
 
-    time.sleep(2)
-
-    ContinueGame = str(input("Type 'PLAY' start the game skipping the intro and tutorial. Type 'LEARN' if you want to read the tutorial and intro of the game "))
-    if ContinueGame.lower() == "play":
-        return 1
-    if ContinueGame.lower() == "learn":
-        print("The objective of the game is to make it out of the dungeon ALIVE.")
-        time.sleep(2)
-        print("You will have to pick between a varity of characters. They all have different stats and attributes before you choose you character you will see a breif overview of them")
+def ContiuneGame():
+    Text = "Press"
 
 Introduction()
