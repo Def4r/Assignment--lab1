923c1c8af7963970bddfa2913872ef52a3c78a10
Def4r <danielnumba1@icloud.com>
2023-10-04 11:45:37 -0400

    Partly finished the intro and working on charSect

--- MainMod.py
+++ MainMod.py
@@ -18,12 +18,13 @@ def Introduction():
         sleep(0.04)
         sys.stdout.write(char)
         sys.stdout.flush()
-    
+
     print("")
     ContinuePrompt = str(input(":>"))
     
     if ContinuePrompt.lower() == "play":
-        return 1
+        return "true"
+    
     if ContinuePrompt.lower() == "learn":
         print("")
         TextI = "The objective of the game is to make it out of the dungeon ALIVE."
@@ -41,14 +42,37 @@ def Introduction():
         print("")
         RougeSum = "Rouge: Fast, aglie and has access to double dice which allows them to travel faster but can't take hits and will die easily"
         print("")
+        time.sleep(1)
         KnightSum = ""
         print("")
+        time.sleep(1)
         TankSum = ""
         print("")
+        time.sleep(1)
         MageSum = ""
         print("")
-        DifficultyChar = ""
+        time.sleep(1)
+        DifficultyChars = ""
         print("")
-        
+        time.sleep(2)
+
+        TextIII = "Now the game will start first you will pick your character then the game i will begin."
+        for char in TextIII:
+            sleep(0.04)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+        print("")
+        return "true"
+
+def characterSection():
+    if Introduction() == "true":
+        TextIV = "Pick you character | Type ....."
+        for char in TextIV:
+            sleep(0.04)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+        print("")
+        PickChar = str(input(":>"))
 
 Introduction()
+characterSection()
