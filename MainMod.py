467e3d0606d3b7e48a2676bf34aa5ada7979c668
Def4r <danielnumba1@icloud.com>
2023-10-04 11:50:00 -0400

    cleaned out code and tested introduction

--- MainMod.py
+++ MainMod.py
@@ -2,22 +2,23 @@ import time
 import random
 import sys
 from time import sleep
+from sys import stdout
 
 def Introduction():
     Title = "||Welcome to Dungeon Hunters||"
     for char in Title:
         sleep(0.09)
-        sys.stdout.write(char)
-        sys.stdout.flush()
+        stdout.write(char)
+        stdout.flush()
 
-    time.sleep(2)
+    sleep(2)
     print("")
 
     ContinueGame = "Type 'PLAY' start the game skipping the intro and tutorial. Type 'LEARN' if you want to read the tutorial and intro of the game"
     for char in ContinueGame:
         sleep(0.04)
-        sys.stdout.write(char)
-        sys.stdout.flush()
+        stdout.write(char)
+        stdout.flush()
 
     print("")
     ContinuePrompt = str(input(":>"))
@@ -30,38 +31,39 @@ def Introduction():
         TextI = "The objective of the game is to make it out of the dungeon ALIVE."
         for char in TextI:
             sleep(0.04)
-            sys.stdout.write(char)
-            sys.stdout.flush()
-        time.sleep(2)
+            stdout.write(char)
+            stdout.flush()
+        sleep(2)
         print("")
         TextII = "You will have to pick between a varity of characters. They all have different stats and attributes before you choose you character you will see a breif overview of them"
         for char in TextII:
             sleep(0.04)
-            sys.stdout.write(char)
-            sys.stdout.flush()
+            stdout.write(char)
+            stdout.flush()
         print("")
         RougeSum = "Rouge: Fast, aglie and has access to double dice which allows them to travel faster but can't take hits and will die easily"
         print("")
-        time.sleep(1)
+        sleep(1)
         KnightSum = ""
         print("")
-        time.sleep(1)
+        sleep(1)
         TankSum = ""
         print("")
-        time.sleep(1)
+        sleep(1)
         MageSum = ""
         print("")
-        time.sleep(1)
+        sleep(1)
         DifficultyChars = ""
         print("")
-        time.sleep(2)
+        sleep(2)
 
-        TextIII = "Now the game will start first you will pick your character then the game i will begin."
+        TextIII = "Now the game will restart going back to the starting menu."
         for char in TextIII:
             sleep(0.04)
-            sys.stdout.write(char)
-            sys.stdout.flush()
+            stdout.write(char)
+            stdout.flush()
         print("")
+        sleep(2)
         return "true"
 
 def characterSection():
@@ -69,8 +71,8 @@ def characterSection():
         TextIV = "Pick you character | Type ....."
         for char in TextIV:
             sleep(0.04)
-            sys.stdout.write(char)
-            sys.stdout.flush()
+            stdout.write(char)
+            stdout.flush()
         print("")
         PickChar = str(input(":>"))
 
