0c777ca222543734e4d16dc0f930ba992cb4a13c
Def4r <danielnumba1@icloud.com>
2023-10-08 20:44:53 -0400

    Removed functions to make it easier to send varia

--- CharacterMod.py
+++ CharacterMod.py
@@ -6,43 +6,43 @@
 # There will also be difficulty characters that you can pick that would make the game easier or harder. They have over turned stats to make that experience. 
 
 #Stats for the rouge:
-    RougeCharHP = 1
-    RougeCharSpeed = "dicesII"
-    RougeCharShield = 0
-    RougeCharAttack = 1
-    RougeCharLuckHit = 2
+RougeCharHP = 1
+RougeCharSpeed = "dicesII"
+RougeCharShield = 0
+RougeCharAttack = 1
+RougeCharLuckHit = 2
 
 #Stats for the Tank:
-    TankCharHP = 2
-    TankCharSpeed = "dicesI"
-    TankCharShield = 2
-    TankCharAttack = 1
-    TankCCharLuckHit = 0
+TankCharHP = 2
+TankCharSpeed = "dicesI"
+TankCharShield = 2
+TankCharAttack = 1
+TankCCharLuckHit = 0
 
 #Stats for the Mage:
-    MageCharHP = 1
-    MageCharSpeed = "pickDices"
-    MageCharShield = 2
-    MageCharAttack = 1
-    MageCharLuckHit = 2
+MageCharHP = 1
+MageCharSpeed = "pickDices"
+MageCharShield = 2
+MageCharAttack = 1
+MageCharLuckHit = 2
 
 #Stats for the Knight
-    KnightCharHP = 2
-    KnightCharSpeed = "pickDices"
-    KnightCharShield = 1
-    KnightCharAttack = 2
-    KnightCharLuckHit = 1
+KnightCharHP = 2
+KnightCharSpeed = "pickDices"
+KnightCharShield = 1
+KnightCharAttack = 2
+KnightCharLuckHit = 1
 
 #Nightmare Characters Stats | Hardmode |
-    NightmareCharHP = 1
-    NightmareCharSpeed = "dicesI"
-    NightmareCharShield = 0
-    NightmareCharAttack = 1
-    NightmareCharLuckHit = 0
+NightmareCharHP = 1
+NightmareCharSpeed = "dicesI"
+NightmareCharShield = 0
+NightmareCharAttack = 1
+NightmareCharLuckHit = 0
 
 #Chosen one Character Stats | Easymode | 
-    EasyCharHp = 2
-    EAsyCharSpeed = "dicesII"
-    EasyCharShield = 2
-    EasyCharAttack = 2
-    EasyCharLuckyHit = 2
+EasyCharHp = 2
+EAsyCharSpeed = "dicesII"
+EasyCharShield = 2
+EasyCharAttack = 2
+EasyCharLuckyHit = 2
